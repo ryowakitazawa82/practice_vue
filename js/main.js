beb7526a781f0ede13b28557ddb176d38248c931
ryowakitazawa82 <ryowa.kitazawa@gmail.com>
2021-10-17 01:27:24 +0900

    renew

--- js/main.js
+++ js/main.js
@@ -11,6 +11,41 @@
 //   });
 // })();
 
+// (function() {
+//   'use strict';
+
+//   var vm = new Vue({
+//     el: '#app',
+//     data: {
+//       newItem: '',
+//       todos: [
+//         'task 1',
+//         'task 2',
+//         'task 3'
+//       ]
+//     },
+//     methods: {
+//     addItem: function(e) {
+//       this.todos.push(this.newItem);
+//       this.newItem = '';
+//     }
+//     }
+//   });
+// })();
+
+// (function() {
+//   'use strict';
+
+//   // two way data binding (to UI)
+
+//   var vm = new Vue({
+//     el: '#app',
+//     data: {
+//       name: 'taguchi'
+//     }
+//   });
+// })();
+
 (function() {
   'use strict';
 
